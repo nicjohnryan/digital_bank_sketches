2755a2f4dcbf8f43a45c887e960a2cde4b86bbde
nicjohnryan <nicthechicken@gmail.com>
2018-10-24 05:19:35 +1000

    scraping pdf example

--- digitalBankNotes.py
+++ digitalBankNotes.py
@@ -22,3 +22,23 @@ for w in words:
 
 plt.hist(scores)    
 
+
+#=========================
+# Scrape PDFs in Python
+#=========================
+
+import PyPDF2
+import re
+
+pdfFileObj = open('tempPublicReport_sthkgzi7ad.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+pageObj = pdfReader.getPage(1)
+
+page = pageObj.extractText()
+
+legalName = re.search(r'Legal name(.*?)ABN', page).group(1)
+ANZSIC = re.search(r'ANZSIC(.*?)\/', page).group(1)
+
+print("Legal Name: {} , ANZSIC Code: {}".format(legalName, ANZSIC))
